1a5b4cd5092491e06a1c8f3a77cc337f6f089f50
CraftyCody123 <ishan.dev2006@gmail.com>
2026-06-19 14:37:48 +0530

    app.jsx updated

--- frontend/ootd-frontend/src/App.jsx
+++ frontend/ootd-frontend/src/App.jsx
@@ -1,47 +1,74 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Auth pages
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
-// Wardrobe pages
 import Wardrobe from "./pages/Wardrobe";
+
 import Upload from "./pages/Upload";
 import Mannequin from "./pages/Mannequin";
 import History from "./pages/History";
-// Components
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
 
-function App() {
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        {/* Auth */}
-        <Route path="/" element={<Login />} />
+
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
         {/* Protected Routes */}
-        <Route path="/profile" element={
-          <ProtectedRoute><Profile /></ProtectedRoute>
-        } />
-        <Route path="/wardrobe" element={
-          <ProtectedRoute><Wardrobe /></ProtectedRoute>
-        } />
-        <Route path="/upload" element={
-          <ProtectedRoute><Upload /></ProtectedRoute>
-        } />
-        <Route path="/mannequin" element={
-          <ProtectedRoute><Mannequin /></ProtectedRoute>
-        } />
-        <Route path="/history" element={
-          <ProtectedRoute><History /></ProtectedRoute>
-        } />
+        <Route
+          path="/wardrobe"
+          element={
+            <ProtectedRoute>
+              <Wardrobe />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/upload"
+          element={
+            <ProtectedRoute>
+              <Upload />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/mannequin"
+          element={
+            <ProtectedRoute>
+              <Mannequin />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <History />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
