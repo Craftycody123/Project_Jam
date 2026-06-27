6ba62a2f98ffb6911f7bd6b15b9586b614c44d36
Ria <riaalphonsamartin03@gmail.com>
2026-06-27 23:55:11 +0530

    added netlify link to main.py

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="OOTD API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://kaleidoscopic-gumdrop-0e2918.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
