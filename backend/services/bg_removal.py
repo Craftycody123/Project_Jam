277bc54ebdc9164f96b45f3eeb51ad1b05be2ea3
CraftyCody123 <ishan.dev2006@gmail.com>
2026-06-27 21:33:15 +0530

    rembg

--- backend/services/bg_removal.py
+++ backend/services/bg_removal.py
@@ -1,12 +1,19 @@
-from rembg import remove, new_session
-
 session = None
 
 def get_session():
     global session
+
     if session is None:
+        from rembg import new_session
         session = new_session("u2netp")
+
     return session
 
+
 def remove_background_bytes(image_bytes):
-    return remove(image_bytes, session=get_session())
+    from rembg import remove
+
+    return remove(
+        image_bytes,
+        session=get_session()
+    )
